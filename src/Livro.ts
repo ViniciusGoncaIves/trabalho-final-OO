744f62b52f05d5a9752e74168cc28078ffbd7b37
Vinicius Goncalves <vinigon01@gmail.com>
2025-11-07 21:02:43 -0300

    Adição dos métodos e getters.

--- src/Livro.ts
+++ src/Livro.ts
@@ -1,18 +1,50 @@
-class Livro{
-    private titulo: string;
-    private autor: string
-    private anoPublicacao: number;
-    private disponivel: boolean;
+class Livro {
+  private titulo: string;
+  private autor: string;
+  private editora: string;
+  private anoPublicacao: number;
+  private disponivel: boolean;
 
-    constructor(titulo: string, autor: string, anoPublicacao: number){
-        this.titulo = titulo;
-        this.autor = autor;
-        this.anoPublicacao = anoPublicacao;
-        this.disponivel = true;
+  constructor(titulo: string,autor: string,editora: string,anoPublicacao: number,disponivel: boolean) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.editora = editora;
+    this.anoPublicacao = anoPublicacao;
+    this.disponivel = disponivel;
+  }
+
+  getTitulo(): string {
+    return this.titulo;
+  }
+
+  getAutor(): string {
+    return this.autor;
+  }
+
+  getEditora(): string {
+    return this.editora;
+  }
+
+  getAnoPublicacao(): number {
+    return this.anoPublicacao;
+  }
+
+  getDisponivel(): boolean {
+    return this.disponivel;
+  }
+
+  emprestar(): void {
+    if (!this.disponivel) {
+      throw new Error(`Livro ${this.titulo} indisponível para empréstimo.`);
     }
+    this.disponivel = false;
+  }
+
+  devolver(): void {
+    if (this.disponivel) {
+      throw new Error("Não foi possível devolver o livro, pois ele não foi emprestado ainda.");
+    }
+    this.disponivel = true;
+  }
 }
 
-let livro1 = new Livro("1984", "George Orwell", 1949);
-let livro2 = new Livro("To Kill a Mockingbird", "Harper Lee", 1960);
-console.log(livro1);
-console.log(livro2);
